beea1c55b19188a92789433f4407dc713402f364
Warden <121907339+wardenmunyua@users.noreply.github.com>
2023-11-05 10:13:51 -0800

    file-picker.py

--- file-picker.py
+++ file-picker.py
@@ -1,4 +1,4 @@
-from tkinter import Tk
+from tkinter import Tk                                      #for version <3 use 'Tkinter' instead of 'tkinter'
 from tkinter.filedialog import askopenfilename
 
 #Tk.withdraw() can be used to prevent the main window from opening
